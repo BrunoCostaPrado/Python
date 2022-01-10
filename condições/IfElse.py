3bd3b9dbc6dca48278917e67c3136f96a4e9ef9c
BrunoCostaPrado <brunocostaprado@gmail.com>
2022-01-10 18:26:51 -0300

    .

---
+++ condições/IfElse.py
@@ -0,0 +1,8 @@
+x=10-1
+
+if x==10:
+    print("igual a 10")
+elif x>10:
+    print("Maior que 10")
+elif x<10:
+    print("Menor que 10")
---
+++ condições/input.py
@@ -0,0 +1,7 @@
+x=name = int(input('Insira um numero\n'))
+if x==10:
+    print("igual a 10")
+elif x>10:
+    print("Maior que 10")
+elif x<10:
+    print("Menor que 10")
