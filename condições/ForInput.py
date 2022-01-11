4ef47973b0b7be97983915edd28a52fa2fa5a789
BrunoCostaPrado <brunocostaprado@gmail.com>
2022-01-11 01:04:51 -0300

    .

---
+++ condições/ForInput.py
@@ -0,0 +1,10 @@
+x= int(input('Insira um numero\n'))
+y=10
+z=30
+for x in range(z):
+    if x==y:
+        print("igual a 10")
+    elif x>y:
+        print("Maior que 10")
+    elif x<y:
+        print("Menor que 10")
--- condições/input.py
+++ condições/input.py
@@ -1,9 +1,10 @@
-x=name = int(input('Insira um numero\n'))
-if x==10:
+x= int(input('Insira um numero\n'))
+y=10
+if x==y:
     print("igual a 10")
-elif x>10:
+elif x>y:
     print("Maior que 10")
-elif x<10:
+elif x<y:
     print("Menor que 10")
 
 # input= inserir algo
