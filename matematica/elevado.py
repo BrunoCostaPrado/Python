5508b803952a3a9ec8f0282df8644274e75e5bcb
BrunoCostaPrado <brunocostaprado@gmail.com>
2022-01-11 18:25:11 -0300

    .

---
+++ matematica/elevado.py
@@ -0,0 +1,3 @@
+x=int(input("insira um numero:\n"))
+y=int(input("insira o expoente:\n"))
+print(str(x) + ' ^ ' +str(y)+' = '+ str(pow(x,y)))
---
+++ matematica/math.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+#Math
+print(50 + 50) #add
+print(50 - 50) #subtract
+print(50 * 50) #multiply
+print(50 / 50) #divide
+print(50 + 50 -50 *50 / 50) #?
+print(50 ** 50) #exponecial
+print(50 % 6) #modulo
+print(50/6) #com resto
+print(50//6) #sem resto
