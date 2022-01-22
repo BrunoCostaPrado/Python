e27a07a80833480bc0b9958cf9db3d11374e0c15
BrunoCostaPrado <brunocostaprado@gmail.com>
2022-01-22 01:55:46 -0300

    0

--- ManipularArquivos/inventario.py
+++ ManipularArquivos/inventario.py
@@ -1,26 +1,23 @@
+# from Funcao.Funcao_Arquivos import *
+# inventario={}
+# opcao=chamarMenu()
+# while opcao>0 and opcao<4:
+#     if opcao==1:
+#         registrar(inventario)
+#     elif opcao==2:
+#         registrar(inventario)
+#     elif opcao==3:
+#         print(exibir())
+#     opcao=chamarMenu()
+
+from Funcao.Funcao_Arquivos import *
 inventario={}
-opcao=int(input("Digite: "
-"<1> para  registar ativo"
-"<2> para persisitir em arquivo"
-"<3> para exibir ativos armazenados:"))
+opcao=chamarMenu()
 while opcao>0 and opcao<4:
     if opcao==1:
-        resp="S"
-        while resp=="S":
-            inventario[input("Digite um numero ")]=[
-            input("digite a data da ultima atualização: ")  ,
-            input("Digite a descrição: "),
-            input("Digite o departamento: ")]
-            resp=input("Digite <S> para continuar.").upper()
+        registrar(inventario)
     elif opcao==2:
-        with open("Inventario.csv","a") as inv:
-            for chave, valor in inventario.items():
-                inv.write(chave+";"+valor[0]+";"+valor[1]+";"+valor[2]+"")
-                print("Persistido com sucesso!")
+        persistir(inventario)
     elif opcao==3:
-        with open("Inventario.csv","r")as inv:
-            print(inv.readlines())
-    opcao=int(input("Digite: "
-    "<1> para registrar ativo"
-    "<2> para persisitr em arquivo"
-    "<3> para exibir ativos armazenados: "))
+        print(exibir())
+    opcao = chamarMenu()
